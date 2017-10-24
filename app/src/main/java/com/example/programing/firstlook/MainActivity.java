1216f66b09f3af6b780c7d9d8bc6b38838614917
Bitterpilot <nathanminissale@gmail.com>
2017-10-24 21:40:24 +0800

    Alter layout objects

---
+++ app/src/main/java/com/example/programing/firstlook/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.programing.firstlook;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+//import widget
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    //set a variable to make calling easier
+    //   V  remember to prefer private calls
+    private Button hiButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.new_layout);
+        //identify object                  ID is in the top of the properties panel in the
+        //                                 V     layout.xml
+        hiButton = (Button) findViewById(R.id.hiButton);
+        //alter object
+        hiButton.setText("hello button");
+        hiButton.setTextColor(Color.BLUE);
+
+    }
+}
